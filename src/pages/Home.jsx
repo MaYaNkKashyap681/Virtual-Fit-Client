cf1345d901cca95fa2b81a4efbf1c608240e7c4c
Mayank Kashyap <mayankkashyap681@Mayanks-MacBook-Air.local>
2024-04-07 23:39:12 +0530

    Adding Try On Code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="h-screen w-screen flex items-center justify-center bg-[#0f0f0f] relative text-white">
       <div className="absolute h-[20rem] w-[20rem] rounded-full bg-blue-500 blur-[16rem] z-[1]"></div>
       <div className="text-center z-[4]">
-        <h1 className="text-4xl font-bold mb-4">Welcome to Virtua Fit</h1>
+        <h1 className="text-4xl font-bold mb-4">Welcome to Virtual Fit</h1>
         <p className="text-lg mb-8">
           Experience the future of shopping with our AR-based solution. Try on
           clothes virtually and customize them using 3D models.
